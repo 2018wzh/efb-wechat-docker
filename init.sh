a21bde4c177a077226b9d85a2e887513d939765d
2018wzh <wzh20070720@outlook.com>
2023-01-26 13:11:17 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/with-contenv bash
-sed -i "s/token123456/$TOKEN/g" /opt/app/ehforward_config/profiles/${PROFILE}/blueset.telegram/config.yaml 
-sed -i "s/admin123456/$ADMIN/g" /opt/app/ehforward_config/profiles/${PROFILE}/blueset.telegram/config.yaml
 ehforwarderbot -p ${PROFILE}
